9241ceaa8c74bb26a958eaea4ee32ab416ee4cb5
delinuxist <delinuxist@gmail.com>
2020-11-09 20:45:09 +0000

    mission

--- app.js
+++ app.js
@@ -12,4 +12,16 @@ $(document).ready(function () {
       $(".nav-menu").removeClass("custom-navbar");
     }
   });
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    console.log(position);
+    if (position >= 600) {
+      $(".camera-img").addClass("fromLeft");
+      $(".mission-text").addClass("fromRight");
+    } else {
+      $(".camera-img").removeClass("fromLeft");
+      $(".mission-text").removeClass("fromRight");
+    }
+  });
 });
